f966db99fd0de7b36efd343455992926606c7ded
Mark Bieganek <mark.bieganek@telus.com>
2023-01-23 16:15:33 -0700

    Get player names on intialization

--- game.rb
+++ game.rb
@@ -1,4 +1,21 @@
 class Game
+  def initialize
+    puts ("TwO-O-Player Math Game")
+    puts ("Challenge a friend to a math battle for the ages!")
+
+    puts ("Enter P1 name:")
+    player_1_name = gets.chomp
+    
+    puts ("Enter P2 name:")
+    player_2_name = gets.chomp
+
+    player_1 = Player.new (player_1_name)
+    player_2 = Player.new (player_2_name)
+
+    puts ("The names have been locked!")
+    puts ("#{player_1.name} vs #{player_2.name}")
+  end
+
   def to_s
     "I'm a Game"
   end
