5c92c868d134ebf8ec4c09c8a3cc55cc0740fddd
Mark Bieganek <mark.bieganek@telus.com>
2023-01-23 16:17:05 -0700

    Define player methods

--- player.rb
+++ player.rb
@@ -1,4 +1,19 @@
 class Player
+  attr_accessor :name, :life
+
+  def initialize(name)
+    @name = name
+    @life = 3
+  end
+  
+  def lose_a_life
+    @life -= 1
+  end
+
+  def alive?
+    @life > 0
+  end
+
   def to_s
     "I'm a Player"
   end
