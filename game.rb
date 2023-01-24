b511ad51961a6a4a0b9508fa44f4b5af9a6161c7
Mark Bieganek <mark.bieganek@telus.com>
2023-01-23 19:04:38 -0700

    Move players to array to prepare for order of play handling

--- game.rb
+++ game.rb
@@ -1,28 +1,62 @@
 class Game
-  attr_reader = :player_1, :player_2, :current_player, :question, :answer
+  attr_reader = :players, :question, :answer
 
   INITIAL_PLAYER_LIFE = 1
 
   def initialize
+    @players = []
     puts ("TwO-O-Player Math Game")
     puts ("Challenge a friend to a math battle for the ages!")
 
     puts ("Enter P1 name:")
-    @player_1_name = gets.chomp
+    player_1_name = gets.chomp
     
     puts ("Enter P2 name:")
-    @player_2_name = gets.chomp
+    player_2_name = gets.chomp
 
-    @player_1 = Player.new(@player_1_name, INITIAL_PLAYER_LIFE)
-    @player_2 = Player.new(@player_2_name, INITIAL_PLAYER_LIFE)
+    player_1 = Player.new(player_1_name, INITIAL_PLAYER_LIFE)
+    player_2 = Player.new(player_2_name, INITIAL_PLAYER_LIFE)
+    @players << player_1 
+    @players << player_2
+    @current_player = 0
 
     puts ("The names have been locked!")
-    puts ("#{@player_1.name} vs #{@player_2.name}")
-
-    self.new_round()
+    message = ""
+    total_players = @players.length
+    @players.each_with_index do |player, current|
+      message += "#{player.name}"
+      if current < total_players
+        message += " vs "
+      end
+    end
+    puts message
+    
+    self.new_round(0)
   end
 
   private
+
+  def new_round(player_number)
+    @current_player = @players[player_number]
+    new_question
+    ask_question(@current_player.name)
+
+    if correct?
+      puts ("YES! You are correct.")
+    else
+      @current_player.lose_a_life
+      puts ("Seriously? No!")
+    end
+    
+    if continue?
+      say_score
+      puts ("----- NEW TURN -----")
+    else
+      say_winner
+      puts ("----- GAME OVER -----")
+      puts ("Good bye!")
+    end
+  end 
 
   def new_question
     @question = Question.new
@@ -37,34 +71,56 @@ class Game
     @question.correct?(@answer)
   end
 
-  def new_round
-    @current_player = @player_1
-    new_question
-    ask_question(@current_player.name)
-
-    if correct?
-      puts ("YES! You are correct.")
-    else
-      @current_player.lose_a_life
-      puts ("Seriously? No!")
-    end
-
-    continue_playing = @player_1.alive? && @player_2.alive?
-    if continue_playing
-      puts ("P1: #{@player_1.say_score} vs P2: #{@player_2.say_score}")
-      puts ("----- NEW TURN -----")
-    else
-      if @player_1.life === @player_2.life
-        puts ("Gameplay ends with a score of P1: #{@player_1.say_score} vs P2: #{@player_2.say_score}")
-      elsif @player_1.life > @player_2.life
-        puts ("Player 1 wins with a score of #{@player_1.say_score}")
-      else
-        puts ("Player 2 wins with a score of #{@player_2.say_score}")
+  def continue?
+    @players.each do |player|
+      if !player.alive?
+        return false
       end
-      puts ("----- GAME OVER -----")
-      puts ("Good bye!")
     end
-  end 
+    return true
+  end
+
+  def say_score
+    message = ""
+    total_players = @players.length
+    @players.each_with_index do |player, current|
+      message += "#{@player.name}: #{@player.say_score}"
+      if current < total_players
+        message += " vs "
+      end
+    end
+    puts message
+  end
+
+  # def tied?
+  #   score = @players[0].life
+  #   @players.each do |player|
+  #     if player.life !== score
+  #       return false
+  #     end
+  #   end
+  #   return true
+  # end
+
+  def find_winner
+    score = @players[0].life
+    winner = nil
+    @players.each do |player|
+      if player.life > score
+        winner = player
+      end
+    end
+    return winner
+  end
+
+  def say_winner
+    winner = find_winner
+    if winner
+      puts ("#{winner.name} wins with a score of #{winner.say_score}")
+    else
+      puts ("There was no winner")
+    end
+  end
 
   def to_s
     "I'm a Game"
