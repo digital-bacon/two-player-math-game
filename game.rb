6397f6e6a7def2bf43ada68b13bc336da1b66d77
Mark Bieganek <mark.bieganek@telus.com>
2023-01-23 17:47:31 -0700

    Create Game method new_round, abstract some logic to it

--- game.rb
+++ game.rb
@@ -1,4 +1,5 @@
 class Game
+  attr_reader = :player_1, :player_2
 
   INITIAL_PLAYER_LIFE = 1
 
@@ -7,46 +8,51 @@ class Game
     puts ("Challenge a friend to a math battle for the ages!")
 
     puts ("Enter P1 name:")
-    player_1_name = gets.chomp
+    @player_1_name = gets.chomp
     
     puts ("Enter P2 name:")
-    player_2_name = gets.chomp
+    @player_2_name = gets.chomp
 
-    player_1 = Player.new(player_1_name, INITIAL_PLAYER_LIFE)
-    player_2 = Player.new(player_2_name, INITIAL_PLAYER_LIFE)
+    @player_1 = Player.new(@player_1_name, INITIAL_PLAYER_LIFE)
+    @player_2 = Player.new(@player_2_name, INITIAL_PLAYER_LIFE)
 
     puts ("The names have been locked!")
-    puts ("#{player_1.name} vs #{player_2.name}")
+    puts ("#{@player_1.name} vs #{@player_2.name}")
 
+    self.new_round()
+  end
+
+  def new_round
+    # Generate a new question, and ask the player
     question = Question.new
     puts ("Player 1: #{question.ask}")
-    player_answer = gets.chomp.to_i
+    @player_answer = gets.chomp.to_i
     
-    correct = question.correct?(player_answer)
+    # Check if the answer is correct
+    correct = question.correct?(@player_answer)
     if !correct
-      player_1.lose_a_life
+      @player_1.lose_a_life
       puts ("Seriously? No!")
     else
       puts ("YES! You are correct.")
     end
 
-    continue_playing = player_1.alive? && player_2.alive?
+    continue_playing = @player_1.alive? && @player_2.alive?
     if continue_playing
-      puts ("P1: #{player_1.say_score} vs P2: #{player_2.say_score}")
+      puts ("P1: #{@player_1.say_score} vs P2: #{@player_2.say_score}")
       puts ("----- NEW TURN -----")
     else
-      if player_1.life === player_2.life
-        puts ("Gameplay ends with a score of P1: #{player_1.say_score} vs P2: #{player_2.say_score}")
-      elsif player_1.life > player_2.life
-        puts ("Player 1 wins with a score of #{player_1.say_score}")
+      if @player_1.life === @player_2.life
+        puts ("Gameplay ends with a score of P1: #{@player_1.say_score} vs P2: #{@player_2.say_score}")
+      elsif @player_1.life > @player_2.life
+        puts ("Player 1 wins with a score of #{@player_1.say_score}")
       else
-        puts ("Player 2 wins with a score of #{player_2.say_score}")
+        puts ("Player 2 wins with a score of #{@player_2.say_score}")
       end
       puts ("----- GAME OVER -----")
       puts ("Good bye!")
     end
-
-  end
+  end 
 
   def to_s
     "I'm a Game"
